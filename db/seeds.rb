40884a0d7f05a750a0d07bb6d1d63d86f12ab43e
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-15 21:21:29 +0800

    adds faker gem for dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+    User.create(name: Faker::Name.first_name)
+end
+
+2.times do
+    Board.create(name: Faker::Game.title)
+end
+
+50.times do
+    entry = Entry.create(
+        score: Faker::Number.number(digits: 4),
+        user_id: Faker::Number.between(from: 1, to: 10),
+        board_id: Faker::Number.between(from: 1, to: 5)
+    )
+
+    entry.scored_at = entry.created_at
+    entry.save
+end
