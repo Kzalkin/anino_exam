fd5bd305bb89d7ed1accdf29fa64dc5f882a8f90
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-15 15:23:58 +0800

    adds leaderboard and admin leaderboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: %i[create show]
+  resources :leaderboard, only: %i[show]
+
+  namespace :admin do
+    resources :leaderboard, only: :index
+  end
 end
