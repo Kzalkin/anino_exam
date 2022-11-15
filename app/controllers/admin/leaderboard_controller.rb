63dcc79a52c7fc5a07962a016fd3df707f5bae80
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-15 15:30:17 +0800

    adds create method in admin leaderboard controller

--- app/controllers/admin/leaderboard_controller.rb
+++ app/controllers/admin/leaderboard_controller.rb
@@ -1,4 +1,17 @@
 class Admin::LeaderboardController < ApplicationController
-    def index
+    def create
+        @leaderboard = Leaderboard.new(leaderboard_params)
+
+        if @leaderboard.save
+            render json: @leaderboard
+        else
+            render json: {status: "Error", errors: @leaderboard.errors}
+        end
+    end
+
+    private
+
+    def leaderboard_params
+        params.permit(:name)
     end
 end
